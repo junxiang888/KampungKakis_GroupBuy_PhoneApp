71123380ed10bbad3b30e9d8bb57977889e6fc41
cwk30 <james.rokz.007@gmail.com>
2021-06-15 18:50:23 +0800

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>James the best :)</Text>
+      <Text>Please code faster</Text>
       <StatusBar style="auto" />
     </View>
   );
