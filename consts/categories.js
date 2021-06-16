95c1bcfb7941d34b76374ebff002251d50101f66
junxiang888 <junxiangteo97@gmail.com>
2021-06-17 06:07:12 +0800

    last

--- consts/categories.js
+++ consts/categories.js
@@ -1,8 +1,8 @@
 const categories = [
-  {id: '1', name: 'pizza', image: require('../assets/catergories/pizza.png')},
-  {id: '2', name: 'Burger', image: require('../assets/catergories/burger.png')},
-  {id: '3', name: 'Sushi', image: require('../assets/catergories/sushi.png')},
-  {id: '4', name: 'Salad', image: require('../assets/catergories/salad.png')},
+  {id: '1', name: 'Chinese', image: require('../assets/catergories/kwayteow.png')},
+  {id: '2', name: 'Japanese', image: require('../assets/catergories/japanese.png')},
+  {id: '3', name: 'Western', image: require('../assets/catergories/western.png')},
+  {id: '4', name: 'Malay', image: require('../assets/catergories/nasilemark.png')},
 ];
 
 export default categories;
---
+++ consts/grocery.js
@@ -0,0 +1,32 @@
+const grocery = [
+  {
+    id: '1',
+    name: 'ABC Market',
+    ingredients: 'Carrot',
+    price: 1,
+    image: require('../assets/catergories/carrot.jpg'),
+  },
+  {
+    id: '2',
+    name: 'Tekka Market',
+    ingredients: 'Broccoli',
+    price: 2.10,
+    image: require('../assets/catergories/broccoli.jpg'),
+  },
+  {
+    id: '3',
+    name: 'Tekka Market',
+    ingredients: 'Egg plant',
+    price: 1.10,
+    image: require('../assets/catergories/eggplant.jpg'),
+  },
+  {
+    id: '4',
+    name: 'Tekka Market',
+    ingredients: 'Red Capsicum',
+    price: 2.00,
+    image: require('../assets/catergories/red_cap.jpg'),
+  },
+];
+
+export default grocery;
